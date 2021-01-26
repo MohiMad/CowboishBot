f31ae3d661dfc79f43c2d3da4194e50b9e031f4c
MohiMoo <56826704+MohiMad@users.noreply.github.com>
2021-01-26 15:09:51 +0100

    Smooth scrolling using javascript

--- src/js/switchPage.js
+++ src/js/switchPage.js
@@ -1,10 +1,28 @@
-
 function switchPage(id) {
     const features = document.getElementById("features");
     const commands = document.getElementById("commands");
     const mohi = document.getElementById("mohi");
 
     const page = document.getElementById(id.replace("#", ""));
+
+    if (id !== "#") {
+        page.addEventListener("click", function (event) {
+
+            event.preventDefault()
+            window.scroll({
+                top: document.getElementById(id.slice(1)).offsetTop,
+                behavior: "smooth"
+            });
+        });
+
+    } else {
+
+        window.scroll({
+            top: 0,
+            behavior: "smooth"
+        });
+    }
+
 
     if (id === "#") {
         features.style.display = "block";
@@ -13,44 +31,24 @@ function switchPage(id) {
         commands.style.display = "none";
         mohi.style.display = "none";
     }
-    if (id === "features") {
+    if (id === "#features") {
         page.style.display = "block";
 
 
         commands.style.display = "none";
         mohi.style.display = "none";
     }
-    if (id === "commands") {
+    if (id === "#commands") {
         page.style.display = "block";
 
         features.style.display = "none";
         mohi.style.display = "none";
     }
-    if (id === "mohi") {
+    if (id === "#mohi") {
 
         page.style.display = "block";
         commands.style.display = "none";
         features.style.display = "none";
 
-    }
-
-    let top = 0;
-
-    if (id !== "#") {
-        top = page.offsetTop;
-        console.log(top);
-
-        page.addEventListener("click", function (event) {
-            event.preventDefault();
-            window.scrollTo({
-                top: top,
-                behavior: "smooth"
-            });
-        });
-    }else {
-        window.scrollTo({
-            top: 0,
-            behavior: "smooth"
-        });
     }
 }
