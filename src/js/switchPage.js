13b05af1ebd188ce559a058b420101a3145428a6
MohiMoo <56826704+MohiMad@users.noreply.github.com>
2021-01-26 15:53:28 +0100

    Better smooth scrolling | Created an img folder

--- src/js/switchPage.js
+++ src/js/switchPage.js
@@ -5,21 +5,10 @@ function switchPage(id) {
 
     const page = document.getElementById(id.replace("#", ""));
 
-    if (id !== "#") {
+    if (page != null) {
         page.addEventListener("click", function (event) {
+            event.preventDefault();
 
-            event.preventDefault()
-            window.scroll({
-                top: document.getElementById(id.slice(1)).offsetTop,
-                behavior: "smooth"
-            });
-        });
-
-    } else {
-
-        window.scroll({
-            top: 0,
-            behavior: "smooth"
         });
     }
 
@@ -51,4 +40,35 @@ function switchPage(id) {
         features.style.display = "none";
 
     }
+
+    if (id !== "#") {
+        const targetPosition = page.offsetTop ? page.offsetTop : 0;
+        const startPosition = window.pageYOffset;
+        const distance = targetPosition - startPosition;
+        const duration = 1000;
+        let start = null;
+
+        window.requestAnimationFrame(step);
+
+        function step(timestamp) {
+            if (!start) start = timestamp;
+            const progress = timestamp - start;
+
+            window.scrollTo(0, easeInOutCubic(progress, startPosition, distance, duration));
+            if (progress < duration) window.requestAnimationFrame(step);
+
+        }
+    } else {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    }
+
+    function easeInOutCubic(t, b, c, d) {
+        t /= d / 2;
+        if (t < 1) return c / 2 * t * t * t + b;
+        t -= 2;
+        return c / 2 * (t * t * t + 2) + b;
+    };
 }
