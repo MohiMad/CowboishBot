cdf05f247be0c9f3c3a5b1419758f54d0fe63f2c
MohiMoo <56826704+MohiMad@users.noreply.github.com>
2021-02-18 20:48:01 +0100

    Adjusted website for mobile users

--- assets/js/chair.js
+++ assets/js/chair.js
@@ -1,6 +1,10 @@
 const chair = document.getElementById("chairButton");
 
+
 function scrollFunction() {
+
+  if (window.innerWidth < 630 || document.documentElement.clientWidth < 630 || document.body.clientWidth < 630) return chair.style.display = "none";
+
   if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
     if (document.body.offsetTop > document.querySelector('footer').offsetTop || document.documentElement.offsetTop > document.querySelector('footer').offsetTop) {
       chair.style.display = "none";
